826501dabadf12914e11696c84d12363208a7ee1
Mateusz Bienkowski <ferumdriel@gmail.com>
2019-02-18 15:05:35 +0100

    moved getting dice side out of throw to control randomness

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import abc
 import enum
 import random
 
-
 class Animal(enum.Enum):
     RABBIT = 1
     SHEEP = RABBIT.value * 6
@@ -19,5 +18,8 @@ class Dice:
     def __init__(self, animals: list):
         self.animals = animals
 
+    def _get_side_animal(self, side: int):
+        return self.animals[side]
+
     def throw(self):
-        return self.animals[random.randint(0, len(self.animals) - 1)]
+        return self._get_side_animal(random.randint(0, len(self.animals) - 1))
