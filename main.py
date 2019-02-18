58aeeff1b50666aadcb46330c4b960ec417a0fad
Mateusz Bienkowski <ferumdriel@gmail.com>
2019-02-18 14:54:22 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import abc
+import enum
+import random
+
+
+class Animal(enum.Enum):
+    RABBIT = 1
+    SHEEP = RABBIT.value * 6
+    SMALL_DOG = SHEEP.value
+    PIG = SHEEP.value * 2
+    COW = PIG.value * 3
+    BIG_DOG = COW.value
+    HORSE = COW.value * 2
+    FOX = 0
+    WOLF = 0
+
+
+class Dice:
+    def __init__(self, animals: list):
+        self.animals = animals
+
+    def throw(self):
+        return self.animals[random.randint(0, len(self.animals) - 1)]
