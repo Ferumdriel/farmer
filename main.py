f7a60151537e70d46ed066902c94cc94af44a7d9
Mateusz Bienkowski <ferumdriel@gmail.com>
2019-02-19 22:10:25 +0100

    implemented simplest Farm functionality, refactored Farm test

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ class Breeder:
     def count_new_animals(present_animals, animals_on_sides):
         return 0 if animals_on_sides == 0 else int((present_animals + animals_on_sides) / 2)
 
+class Farm:
+    def __init__(self):
+        self.animals = [0,0,0]
+
+    def breed_animals(self, dice_animals):
+        new_animals = [Breeder.count_new_animals(ex, dice) for ex, dice in zip(self.animals, dice_animals)]
+        for i, to_add in enumerate(new_animals):
+            self.animals[i] += to_add
+
+
 
 class Dice:
     def __init__(self, animals: list):
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Breeder
+from main import Breeder, Farm
 
 
 class BreederTest(unittest.TestCase):
@@ -12,10 +12,12 @@ class BreederTest(unittest.TestCase):
 
 class FarmTest(unittest.TestCase):
     def test_animals_breeding(self):
-        farm = Farm()
-        existing_animals = [2, 0, 2]
-        dice_animals = [1, 2, 0]
-        new_animals = [Breeder.count_new_animals(ex, dice) for ex, dice in zip(existing_animals, dice_animals)]
-        expected_total_animals = [3, 1, 2]
-        counted_total_animals = farm.breed_animals(dice_animals)
-        self.assertEquals(expected_total_animals, self.total_animals)
+        def _check_farm(animals, dice_animals, expected_total_animals):
+            farm = Farm()
+            farm.animals = animals
+            dice_animals = dice_animals
+            expected_total_animals = expected_total_animals
+            farm.breed_animals(dice_animals)
+            self.assertEqual(expected_total_animals, farm.animals)
+
+        _check_farm(animals=[2, 0, 2], dice_animals=[1, 2, 0], expected_total_animals=[3, 1, 2])
